63e66d7c5d17285b9a6c0f1b6b96f5eb36f9eca5
Daniel Pressner <danielpressner@daniels-macbook-pro.local>
2019-04-10 22:54:33 +0200

    Initial Commit

---
+++ photoUtilities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoUtilities
+//
+//  Created by Daniel Pressner on 10.04.19.
+//  Copyright © 2019 Daniel Pressner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
